b50e88a742baf7894e22ea3c4321e2fd16cca286
Matias Leitner <leitnermatias22@gmail.com>
2022-11-28 14:41:29 -0300

    Add BarChart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Bar
+        :styles="{width: '400px', height: '400px'}"
+        :chart-data="data"
+        >
+        </Bar>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {Bar} from "vue-chartjs"
+import { Chart as ChartJS, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Tooltip, BarElement, CategoryScale, LinearScale)
+
+defineProps<{
+    data: {
+        labels: string[],
+        datasets: {label?: string, backgroundColor?: string, data: number[]}[]
+    }
+}>()
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,72 @@
 <template>
     <Panel>
-        <div>
-            <div>
-                <h3>Last transactions</h3>
-                <div>
-                    
-                </div>
-            </div>
+        <div class="container">
+            <BarChart :data="{
+                labels: ['Transactions by Currency'],
+                datasets: currencyTotals
+            }" />
         </div>
     </Panel>
 </template>
 
 <script setup lang="ts">
 import Panel from '../components/Panel.vue';
+import BarChart from '../components/BarChart.vue';
+import { selectAndFilter, dbCall } from '../service/database';
+import { store } from '../globals';
+import { onMounted, ref } from 'vue';
+import { Currency, Transaction } from '../types';
+
+const transactions = ref<Transaction[]>([])
+const currencyTotals = ref<{label: string, data: number[]}[]>([])
+const currencies = ref<Currency[]>([])
+
+onMounted(async () => {
+    await getTransactions();
+    await getCurrencies();
+})
+
+async function getTransactions() {
+    const transactionsQuery = selectAndFilter(
+        'transactions',
+        ['*']
+    )
+
+    transactions.value = await dbCall<Transaction[]>(transactionsQuery, store.db) || [];
+}
+
+async function getCurrencies() {
+    const query = selectAndFilter(
+        'currency',
+        ['*']
+    )
+
+    currencies.value = await dbCall(query, store.db) || [];
+
+    let totals: {label: string, data: number[]}[] = []
+
+    currencies.value.forEach(currency => {
+        const total = transactions.value
+        .filter(transaction => transaction.currency === currency.id)
+        .reduce((sumTotal, currentTransaction) => sumTotal + currentTransaction.cost, 0)
+
+        totals.push({
+            label: currency.symbol,
+            data: [total]
+        })
+    })
+
+    currencyTotals.value = totals;
+}
+
 
 
 </script>
 
 <style scoped>
-
+.container {
+    display: flex;
+    gap: 50px;
+    justify-content: center;
+}
 </style>
